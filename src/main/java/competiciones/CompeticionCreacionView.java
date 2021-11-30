e9932c3c22036d4b658ff0c3c55a0b3c5ee4e8dd
CarolinaUniovi <78871007+CarolinaUniovi@users.noreply.github.com>
2021-11-30 11:35:04 +0100

    pequeño error

--- src/main/java/competiciones/CompeticionCreacionView.java
+++ src/main/java/competiciones/CompeticionCreacionView.java
@@ -21,7 +21,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
-import clasificacion.CategoriasView;
+import categorias.CategoriasView;
 import giis.demo.util.Util;
 
 @SuppressWarnings("serial")
